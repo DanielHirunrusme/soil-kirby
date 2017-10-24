4c4687d1edbf383512948e7a81aeb93af2a33f7f
DanielHirunrusme <daniel@halfhalf.us>
2017-10-23 20:04:54 -0500

    fixed email server

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -4,18 +4,6 @@
 
 <main class="content main" role="main">
     
-  <?php 
-      ini_set( 'display_errors', 1 );
-      error_reporting( E_ALL );
-      $from = "daniel@halfhalf.us";
-      $to = "daniel@halfhalf.us";
-      $subject = "PHP Mail Test script";
-      $message = "This is a test to check the PHP Mail functionality";
-      $headers = "From:" . $from;
-      mail($to,$subject,$message, $headers);
-      echo "Test email sent";
-  ?>  
-    
   <div class="content-inner">
     
     <div class="block">
