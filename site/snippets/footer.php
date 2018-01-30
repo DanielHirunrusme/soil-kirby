f8a88b8bd70ba493a9451a586b2204420c56efe5
DanielHirunrusme <daniel@halfhalf.us>
2018-01-30 13:56:50 -0600

    added fastclick

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -12,6 +12,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="http://vjs.zencdn.net/6.4.0/video.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"></script>
+<script type="text/javascript">
+  if ('addEventListener' in document) {
+  	document.addEventListener('DOMContentLoaded', function() {
+  		FastClick.attach(document.body);
+  	}, false);
+  }
+</script>
+  
 <?= js('assets/js/vendor.min.js') ?>
 <?= js('assets/main.min.js') ?>
 
